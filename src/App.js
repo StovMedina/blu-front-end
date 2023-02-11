f3e28e21cb5e4618b12c8388fb820a749ad1b356
panchoman32 <estebandido32@hotmail.com>
2023-02-10 22:52:16 -0700

    button component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
+import BluButton from "./components/Button/BluButton";
 
 function App() {
-  return <div className="App">hello world 02</div>;
+  return (
+    <div className="App">
+      hello world 02
+      <BluButton text="Blu Button" variant="danger" />
+    </div>
+  );
 }
 
 export default App;
