48cf2cae8916455393627dce44a03f4c1ac33586
panchoman32 <estebandido32@hotmail.com>
2023-02-14 15:57:20 -0700

    shopping cart icon added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
+import BluCartButton from "./components/Button/BluCartButton";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      hello world
+      <BluCartButton />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Button/BluCartButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+const BluCartButton = () => {
+  return <FontAwesomeIcon icon={faShoppingCart} />;
+};
+
+export default BluCartButton;
