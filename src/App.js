64049995c2aacc74069873a360232e67b15cfd2f
panchoman32 <estebandido32@hotmail.com>
2023-02-14 16:03:23 -0700

    user idon added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import BluCartButton from "./components/Button/BluCartButton";
+import BluUserButton from "./components/Button/BluUserButton";
 
 function App() {
   return (
     <div className="App">
       hello world
       <BluCartButton />
+      <BluUserButton />
     </div>
   );
 }
