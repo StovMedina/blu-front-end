e2e1626ca999da6cd0daffb00da302330d6ada1a
panchoman32 <estebandido32@hotmail.com>
2023-02-20 21:13:29 -0700

    mercado pago integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
+  // const mp = new MercadoPago("TEST-e877fe73-bb6a-45e5-adb6-de90b04f0862");
   return (
     <div className="App">
       <LandingPage />
