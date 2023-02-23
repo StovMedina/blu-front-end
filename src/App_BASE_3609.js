401f58eff9481a1d7d08947606c04767351a5755
Sof <sofie151091@gmail.com>
2023-02-22 19:30:25 -0600

    remove files that were not used (APP 3609)

--- src/App_BASE_3609.js
+++
@@ -1,7 +0,0 @@
-import "./App.scss";
-
-function App() {
-  return <div className="App">hello world</div>;
-}
-
-export default App;
--- src/App_REMOTE_3609.js
+++
@@ -1,8 +0,0 @@
-import "./App.scss";
-import LogIn from "./pages/Login/LogIn";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
