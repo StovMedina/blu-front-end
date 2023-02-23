d25c8907b8e040430acd210ed1ff6a4371d758d3
panchoman32 <estebandido32@hotmail.com>
2023-02-22 20:48:40 -0700

    landing styles modified

--- src/components/Cards/PrimaryCard/BluCard.js
+++ src/components/Cards/PrimaryCard/BluCard.js
@@ -1,34 +1,34 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { useEffect } from "react";
-import { useMercadopago } from "react-sdk-mercadopago";
+//import { useMercadopago } from "react-sdk-mercadopago";
 
 function BluCard(props) {
-  const mercadopago = useMercadopago.v2(
-    "TEST-e877fe73-bb6a-45e5-adb6-de90b04f0862",
-    {
-      locale: "es-MX",
-    }
-  );
+  // const mercadopago = useMercadopago.v2(
+  //   "TEST-e877fe73-bb6a-45e5-adb6-de90b04f0862",
+  //   {
+  //     locale: "es-MX",
+  //   }
+  // );
 
-  useEffect(() => {
-    if (mercadopago) {
-      mercadopago.checkout({
-        preference: {
-          id: "243699844-eb349d09-e1ac-4d00-b4b4-cca2800cd2e0",
-        },
-        render: {
-          container: "#" + props.id,
-          label: "Pay",
-        },
-      });
-    }
-  }, [mercadopago]);
+  // useEffect(() => {
+  //   if (mercadopago) {
+  //     mercadopago.checkout({
+  //       preference: {
+  //         id: "243699844-eb349d09-e1ac-4d00-b4b4-cca2800cd2e0",
+  //       },
+  //       render: {
+  //         container: "#" + props.id,
+  //         label: "Pay",
+  //       },
+  //     });
+  //   }
+  // }, [mercadopago]);
 
-  console.log(props.id);
+  // console.log(props.id);
 
   return (
-    <Card>
+    <Card className="card-product">
       <Card.Img variant={props.variant} src={props.src} />
       <Card.Body>
         <Card.Title>{props.cardTitle}</Card.Title>
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,132 +1,74 @@
 import React from "react";
 import BluCard from "../../components/Cards/PrimaryCard/BluCard";
 import "./LandingPage.scss";
-// import axios from "axios";
-// import BluButton from "../../components/Button/BluButton";
 import { nanoid } from "nanoid";
 
 const LandingPage = () => {
-  // const [mp, setMp] = useState(null);
+  const data = require("../../MOCK_DATA (1).json");
 
-  // useEffect(() => {
-  //   const script = document.createElement("script");
-  //   script.src = "https://sdk.mercadopago.com/js/v2";
-  //   script.async = true;
-  //   script.onload = () => {
-  //     setMp(window.MercadoPago);
-  //   };
-  //   document.body.appendChild(script);
-  // }, []);
-  // console.log(window);
-
-  // if (!mp) {
-  //   console.log("loading...");
-  // }
-
-  // const mercadoPago = new window.MercadoPago();
-  // console.log(mercadoPago);
-
-  // const mercadopago = new mp.MercadoPago(
-  //   "TEST-e877fe73-bb6a-45e5-adb6-de90b04f0862",
+  // const products = [
   //   {
-  //     locale: "es-MX", // The most common are: 'pt-BR', 'es-AR' and 'en-US'
-  //   }
-  // );
-
-  // const createCheckoutButton = (preferenceId) => {
-  //   // Initialize the checkout
-  //   mercadopago.checkout({
-  //     preference: {
-  //       id: preferenceId,
-  //     },
-  //     render: {
-  //       container: ".button-checkout", // Class name where the payment button will be displayed
-  //       label: "Pay", // Change the payment button text (optional)
-  //     },
-  //   });
-  // };
-
-  // const handleCheckOut = () => {
-  //   const orderData = {
-  //     quantity: 1,
-  //     description: "macizo",
-  //     price: 100,
-  //   };
-  //   const config = {
-  //     headers: {
-  //       // encabezado personalizado
-  //       "Content-Type": "application/json",
-  //     },
-  //   };
-  //   const data = JSON.stringify(orderData);
-  //   axios.post("/payment", data, config);
-  // };
-
-  const products = [
-    {
-      image:
-        "https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Chamarra de mezclilla con bordado",
-      text: "Chamarra con un lindo bordado motivador",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1040424/pexels-photo-1040424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Hoodie para correr",
-      text: "Hoodie ligera ideal para trotar",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/3236651/pexels-photo-3236651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Pareo ligero",
-      text: "Pareo ligero con estampado",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1024036/pexels-photo-1024036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Vestido vaporoso de primavera",
-      text: "Vestido con estampado floral",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Chamarra de mezclilla con bordado",
-      text: "Chamarra con un lindo bordado motivador",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1040424/pexels-photo-1040424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Hoodie para correr",
-      text: "Hoodie ligera ideal para trotar",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/3236651/pexels-photo-3236651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Pareo ligero",
-      text: "Pareo ligero con estampado",
-    },
-    {
-      image:
-        "https://images.pexels.com/photos/1024036/pexels-photo-1024036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Vestido vaporoso de primavera",
-      text: "Vestido con estampado floral",
-    },
-  ];
+  //     image:
+  //       "https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Chamarra de mezclilla con bordado",
+  //     text: "Chamarra con un lindo bordado motivador",
+  //   },
+  //   {
+  //     image:
+  //       "https://images.pexels.com/photos/1040424/pexels-photo-1040424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Hoodie para correr",
+  //     text: "Hoodie ligera ideal para trotar",
+  //   },
+  //   {
+  //     image:
+  //       "https://images.pexels.com/photos/3236651/pexels-photo-3236651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Pareo ligero",
+  //     text: "Pareo ligero con estampado",
+  //   },
+  //   {
+  //     image:
+  //       "https://images.pexels.com/photos/1024036/pexels-photo-1024036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Vestido vaporoso de primavera",
+  //     text: "Vestido con estampado floral",
+  //   },
+  //   {
+  //     image:
+  //       "https://images.pexels.com/photos/1176618/pexels-photo-1176618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Chamarra de mezclilla con bordado",
+  //     text: "Chamarra con un lindo bordado motivador",
+  //   },
+  //   {
+  //     image:
+  //       "https://images.pexels.com/photos/1040424/pexels-photo-1040424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Hoodie para correr",
+  //     text: "Hoodie ligera ideal para trotar",
+  //   },
+  //   {
+  //     image:
+  //       "https://images.pexels.com/photos/3236651/pexels-photo-3236651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Pareo ligero",
+  //     text: "Pareo ligero con estampado",
+  //   },
+  //   {
+  //     image:
+  //       "https://images.pexels.com/photos/1024036/pexels-photo-1024036.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  //     title: "Vestido vaporoso de primavera",
+  //     text: "Vestido con estampado floral",
+  //   },
+  // ];
 
   return (
     <div className="landing-container">
       <main className="landing-main__container">
-        {products.map((product, index) => (
+        {data.map((data, index) => (
           <BluCard
             id={nanoid()}
             key={index}
             variant="top"
-            src={product.image}
-            cardTitle={product.title}
-            cardText={product.text}
-          >
-            <div className="cho-container"></div>
-          </BluCard>
+            src={data.image}
+            cardTitle={data.name}
+            cardText={data.description}
+          ></BluCard>
         ))}
       </main>
       <aside></aside>
