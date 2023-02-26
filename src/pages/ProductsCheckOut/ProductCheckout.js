416b483ae981d90761e88e69792b16f4c3cc077c
Sof <sofie151091@gmail.com>
2023-02-26 16:50:38 -0600

    Radio button was added

--- src/pages/ProductsCheckOut/ProductCheckout.js
+++ src/pages/ProductsCheckOut/ProductCheckout.js
@@ -1,18 +1,51 @@
 import React from "react";
+import RadioButton from "../../components/Button/RadioButton";
+import Form from "react-bootstrap/Form";
+import BluButton from "../../components/Button/BluButton";
+import { useNavigate } from "react-router-dom";
 
 const ProductCheckout = () => {
+  const navigate = useNavigate();
   return (
     <main className="container">
       <h1 className="text-center mb-3">¡Ya casi es tuyo!</h1>
-      <div className="col-12 col-md-6"></div>
+      <div className="row">
+      <div className="col-12 col-md-6">Cards de productos adquiridos</div>
       <div className="col-12 col-md-6">
         <h3 className="text-center">Son correctos tus datos? </h3>
-        <h3 className="text-center">Confirmar</h3>
-        <div>
-            <span>Nombre:</span>
-            <span>Dirección:</span>
-            <span>Teléfono:</span>
+        <h3 className="text-center">Datos de Entrega</h3>
+        <div className="d-flex flex-column">
+          <span>Nombre:</span>
+          <span>Dirección:</span>
+          <span>Teléfono:</span>
         </div>
+        <Form>
+          <RadioButton
+            name="shipping"
+            type="radio"
+            label="Envio estandar gratis"
+          />
+          <RadioButton
+            name="shipping"
+            type="radio"
+            label="Entrega premium $50"
+          />
+          <RadioButton
+            name="shipping"
+            type="radio"
+            label="Entrega en punta de venta gratis"
+          />
+        </Form>
+        <div className="col-12 d-flex align-items-center justify-content-end">
+          <span onClick={() => navigate("/")}>Cancelar</span>
+          <BluButton
+            extraClass="ms-5"
+            actionOnClick={() => navigate("/")}
+            variant="primary"
+            text="Pagar"
+          />
+        </div>
+      </div>
       </div>
     </main>
   );
--- src/routes/ShopRoutes.js
+++ src/routes/ShopRoutes.js
@@ -4,7 +4,7 @@ import { ShopLayout } from "../components/layouts";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import Product from "../pages/product/Product";
 import ShippingDetails from "../pages/ShippingDetails/ShippingDetails";
-
+import ProductCheckout from "../pages/ProductsCheckOut/ProductCheckout";
 export const ShopRoutes = () => {
   return (
     <ShopLayout>
@@ -12,6 +12,7 @@ export const ShopRoutes = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/shippingdetails" element={<ShippingDetails/>} />
+        <Route path="/productcheckout" element={<ProductCheckout/>} />
       </Routes>
     </ShopLayout>
   );
