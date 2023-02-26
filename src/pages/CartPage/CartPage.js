18f91291ce593ec75bfe2a8e877feb80f8f42ec8
panchoman32 <estebandido32@hotmail.com>
2023-02-25 19:50:24 -0700

    cart page added

---
+++ src/pages/CartPage/CartPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CartCard from "../../components/Cards/CartCard/CartCard";
+
+const CartPage = () => {
+  return (
+    <div>
+      <CartCard />
+    </div>
+  );
+};
+
+export default CartPage;
--- src/routes/ShopRoutes.js
+++ src/routes/ShopRoutes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ShopLayout } from "../components/layouts";
+import CartPage from "../pages/CartPage/CartPage";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import Product from "../pages/product/Product";
 
@@ -10,6 +11,7 @@ export const ShopRoutes = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </ShopLayout>
   );
