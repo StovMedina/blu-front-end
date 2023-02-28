ecfd1900c9f76a0a809d3cd712d1fa552857ad30
StovMedina <106270470+StovMedina@users.noreply.github.com>
2023-02-27 18:00:07 -0600

    Revert "cart page added"

--- src/routes/ShopRoutes.js
+++ src/routes/ShopRoutes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ShopLayout } from "../components/layouts";
-import CartPage from "../pages/CartPage/CartPage";
 import LandingPage from "../pages/LandingPage/LandingPage";
 import Product from "../pages/product/Product";
 import ShippingDetails from "../pages/ShippingDetails/ShippingDetails";
@@ -13,7 +12,6 @@ export const ShopRoutes = () => {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/product/:id" element={<Product />} />
-        <Route path="/cart" element={<CartPage />} />
         <Route path="/shippingdetails" element={<ShippingDetails/>} />
         <Route path="/productcheckout" element={<ProductCheckout/>} />
         <Route path="/ordercompleted" element={<Ordercomplet/>}/>
