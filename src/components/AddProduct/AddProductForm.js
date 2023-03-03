e7a7ba504bc48a86ff3bf246352af995f8586844
Sof <sofie151091@gmail.com>
2023-03-03 02:20:46 -0600

    product admin form was updated

--- src/components/AddProduct/AddProductForm.js
+++ src/components/AddProduct/AddProductForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import axios from 'axios'
-import { Form } from 'react-router-dom';
+import { Form } from "react-bootstrap";
 import BluInput from '../Forms/BluInput';
 import BluButton from '../Button/BluButton';
 import apiURL from "../../config";
@@ -26,69 +26,71 @@ const handleChange = (event) => {
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  console.log(product)
   axios.post(`${apiURL}/products`, product)
-    .then((res) => console.log(res.data))
     .catch((error) => console.error(error));
   };
   return (
-    <Form actionOnSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
      <BluInput
      label="Nombre del producto" 
     type="text"
+    name="name"
     value={product.name}
     actionOnChange={handleChange}
      />
 <BluInput
         label="Talla"
+        name="size"
         type="text"
         value={product.size}
         actionOnChange={handleChange}
       />
 <BluInput
         label="Marca"
+        name="brand"
         type="text"
         value={product.brand}
         actionOnChange={handleChange}
       />
 <BluInput
         label="Detalles de reparacion"
+        name="reparationDetails"
         type="text"
         value={product.reparationDetails}
         actionOnChange={handleChange}
       />
   <BluInput
         label="Color"
+        name="color"
         type="text"
         value={product.color}
         actionOnChange={handleChange}
       />
     <BluInput
         label="Imagen"
+        name="image"
         type="text"
         value={product.image}
         actionOnChange={handleChange}
       />
-
-<BluInput
-        label="Nombre del producto"
-        type="text"
-        value={product.name}
-        actionOnChange={handleChange}
-      />
       <BluInput
         label="Cantidad"
-        type="text"
+        name="quantity"
+        type="number"
         value={product.quantity}
         actionOnChange={handleChange}
       />
       <BluInput
         label="Precio"
+        name="price"
         type="text"
         value={product.price}
         actionOnChange={handleChange}
       />
       <BluInput
         label="Descripción"
+        name="description"
         type="text"
         value={product.description}
         actionOnChange={handleChange}
