bad7a4a4ea5c63f647c25953a634bf2d90c3b452
panchoman32 <estebandido32@hotmail.com>
2023-03-05 21:42:36 -0700

    setting usestate and useeffect on vertical sidebar

--- src/components/GeneralMenu/VerticalSideBar.js
+++ src/components/GeneralMenu/VerticalSideBar.js
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 import Nav from "react-bootstrap/Nav";
 
 function VerticalSideBar(options, props) {
+  const [urlParamName, setUrlParamName] = useState("");
+
+  useEffect(() => {}, [urlParamName]);
+
   const handleClick = (key) => {
     const url = new URL(window.location);
     url.searchParams.set("link", key);
     window.history.pushState({}, "", url);
+    let params = new URLSearchParams(document.location.search);
+    let name = params.get("link");
+    setUrlParamName(name);
+    console.log(name, params);
   };
 
   return (
