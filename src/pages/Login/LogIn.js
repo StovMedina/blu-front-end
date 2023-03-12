3fbfd418fb4d7c53c211b20fa6fc056abcaedd81
Sof <sofie151091@gmail.com>
2023-03-11 21:22:52 -0600

    adding style to login page

--- src/pages/Login/LogIn.js
+++ src/pages/Login/LogIn.js
@@ -3,13 +3,14 @@ import BluInput from "../../components/Forms/BluInput";
 import { Form } from "react-bootstrap";
 import BluButton from "../../components/Button/BluButton";
 import loginSvg from "../../../src/media/login.svg";
-import BluIlustration from "../../components/Images/BluIlustration";
+// import BluIlustration from "../../components/Images/BluIlustration";
 import { useFormik } from "formik";
 import schema from "../../schemas/loginSchema";
 import apiURL from "../../config";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import jwtDecode from "jwt-decode";
+import bluImgLogin from "../../../src/media/bluImgLogin.svg"
 
 const LogIn = () => {
   const navigate = useNavigate();
@@ -44,13 +45,15 @@ const LogIn = () => {
   };
 
   return (
-    <div className="container-fluid login-container d-flex flex-column justify-content-center">
-      <div className="row">
-        <div className="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center">
-          <BluIlustration src={loginSvg} alt="loading" />
-        </div>
-        <div className="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center">
-          <Form className="w-100" onSubmit={formik.handleSubmit}>
+    <div className="border-container_blue container-fluid d-flex">
+      <main className="border-container justify-content-center aling-items-center">
+        <div>
+          <Form onSubmit={formik.handleSubmit}>
+            <div>
+              <h1>Blumode.mx</h1>
+            </div>
+            <h2 className="parrafo1-login">Transformación, rescate e intervención de la mezclilla</h2>
+            <div className="format-input flex-colum">
             <BluInput
               name="email"
               label="Escribe tu e-mail"
@@ -69,16 +72,30 @@ const LogIn = () => {
               value={formik.values.password}
               actionOnChange={formik.handleChange}
             />
-            <BluButton text="Login" type="submit" />
+            <p className="parrafo2-login">No tienes cuenta?, Crea una!</p>
+            </div>
+            <BluButton className="button-login" text="Login" type="submit" />
           </Form>
-          <BluButton
-            text="accede con gugul"
+          <BluButton className="button-googel"
+            text="Accede con google"
             type="button"
             actionOnClick={handleGoogle}
           />
         </div>
-      </div>
+      </main>
+      <aside className="border-container_red ">
+        <img className="log-img" src={bluImgLogin} />
+      </aside>
     </div>
+
+    // <div className="container-fluid login-container d-flex flex-column justify-content-center">
+    //   <div className="row">
+    //     <div className="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center">
+    //       <BluIlustration src={loginSvg} alt="loading" />
+    //     </div>
+    //
+    //   </div>
+    // </div>
   );
 };
 
