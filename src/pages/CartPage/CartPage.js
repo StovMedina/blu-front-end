b5795ee99a7e6d4af5c60554b0f3580340983d5a
panchoman32 <estebandido32@hotmail.com>
2023-03-15 21:10:38 -0700

    deleting products from cart are available

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import BluButton from "../../components/Button/BluButton";
 import CartCard from "../../components/Cards/CartCard/CartCard";
 
@@ -7,6 +8,10 @@ const CartPage = () => {
   const [data, setData] = useState([]);
 
   const [cart, setCart] = useState([]);
+
+  const [deleteProduct, setDeleteProduct] = useState(false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const cartStorage = localStorage.getItem("cart");
@@ -16,19 +21,33 @@ const CartPage = () => {
     axios
       .get(`https://api.aanexo.com/products/cart/id=${idForCheckOut.id}`)
       .then((res) => setData(res.data.payload));
-  }, []);
+  }, [deleteProduct]);
 
-  const handleDelete = (e) => {
-    console.log("uauaua", e.target.value);
+  const handleDelete = (id) => {
+    console.log(id);
+    const newCart = cart.filter((productId) => productId != id);
+    const cartString = JSON.stringify(newCart);
+    console.log(newCart);
+    localStorage.setItem("cart", cartString);
+    setDeleteProduct(!deleteProduct);
   };
 
-  return (
+  return cart.length === 0 ? (
+    <div className="container d-flex flex-column justify-content-center align-items-center">
+      <h1 className="text-center">Aún no hay elementos en el carrito</h1>
+      <BluButton
+        extraClass="mt-4"
+        text="volver a la página"
+        actionOnClick={() => navigate("/blu")}
+      />
+    </div>
+  ) : (
     <div className="container d-flex justify-content-between">
       <main className="container ">
         <div className="cart-header d-flex justify-content-between">
           <h2>Carrito de compras</h2>
           <h2>
-            <span>3</span> productos
+            <span>{data.length}</span> productos
           </h2>
         </div>
         <div className="cards-container">
@@ -45,7 +64,7 @@ const CartPage = () => {
                     price={product.price}
                   >
                     <BluButton
-                      actionOnClick={() => handleDelete()}
+                      actionOnClick={() => handleDelete(product._id)}
                       extraClass="cart-dismiss mt-3"
                       text="eliminar"
                     />
